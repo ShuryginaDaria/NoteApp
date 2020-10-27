d2d8a6c1adaf29432d274982a5404a0a95f3c5d4
Shurygina Darya <tillmedie@gmail.com>
2020-10-27 12:05:46 +0700

    Создам класс NoteCategory

---
+++ NoteAppUI/NoteAppUI/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public Note()
+{
+    _title = "";
+    _category = NoteСategory.Other;
+    _text = "";
+    _creationTime = DateTime.Now;
+    _lastChangeTime = DateTime.Now;
+}
+
+public Note(string title, NoteСategory category, string text)
+{
+    _title = title;
+    _category = category;
+    _text = text;
+    _creationTime = DateTime.Now;
+    _lastChangeTime = DateTime.Now;
+}
