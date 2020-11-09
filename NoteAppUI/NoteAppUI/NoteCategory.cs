5cf7dcb233a33907b0382c12a5bfee94888d1d9f
Shurygina Darya <tillmedie@gmail.com>
2020-11-09 14:51:27 +0700

    Добавлено перечеисление возможных категорий NoteCategory

--- NoteAppUI/NoteAppUI/NoteCategory.cs
+++ NoteAppUI/NoteAppUI/NoteCategory.cs
@@ -1,1 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TUSUR.NoteApp
+{
+    /// <summary>
+    /// Содержит возможные категории заметок.
+    /// </summary>
+    /// <remarks>
+    /// В качестве типа перечислителей используется тип byte, 
+    /// поскольку это позволит сэкономить память при большом количестве заметок 	(порядка 200).
+    /// </remarks>
+    enum NoteСategory : byte
+    {
+        Work,
+        Home,
+        HealseAndSport,
+        People,
+        Document,
+        Finance,
+        Other
+    };
+}
---
+++ NoteAppUI/NoteAppUI/ProjectManajer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace TUSUR.NoteApp
+{
+    /// <summary>
+    /// Класс, реализующий сохранение данных в файл и загрузки из него.
+    /// </summary>
+    internal class ProjectManager
+    {
+        private const string _externalPath = "C:\\Users\\User\\Documents\\NoteApp.notes";
+
+        /// <summary>
+        /// Метод, выполняющий запись в файл
+        /// </summary>
+        /// <param name="projectDictionary">Экземпляр проекта для сериализации</param>
+        public static void ProjectSerialization(Project projectDictionary)
+        {
+            //var myDocumentsPath = Environment.GetFolderPath(SpecialFolder.MyDocuments);
+            // Экземпляр сериалиатора
+            JsonSerializer serializer = new JsonSerializer();
+
+            // Преобразование string в System.IO.Stream
+            byte[] byteArray = Encoding.UTF8.GetBytes(_externalPath);
+            MemoryStream stream = new MemoryStream(byteArray);
+
+            // Открытие потока для записи в файл
+            using (StreamWriter sw = new StreamWriter(@stream))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                // Вызов сериализатора и передача объекта сеиализации
+                serializer.Serialize(writer, projectDictionary);
+            }
+        }
+
+        /// <summary>
+        /// Метод, выполняющий чтение из файла
+        /// </summary>
+        /// <returns>Экземпляр проекта, считанный из файла</returns>
+        public static Project ProjectDeserialization()
+        {
+            // Экземпляр сериализатора
+            JsonSerializer serializer = new JsonSerializer();
+
+            // Переменная для помещения результата
+            Project project = null;
+
+            // Преобразование string в System.IO.Stream
+            byte[] byteArray = Encoding.UTF8.GetBytes(_externalPath);
+            MemoryStream stream = new MemoryStream(byteArray);
+
+            // Открытие потока для чтения и файла
+            using (StreamReader sr = new StreamReader(@stream))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                // Вызов десериализации
+                project = (Project)serializer.Deserialize(reader);
+            }
+
+            return project;
+        }
+    }
+}
